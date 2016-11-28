b14dbd5db8e7323fdc7c2f8aa7933a1b550341e4
novneetnov <novneet346iith@gmail.com>
2016-11-28 20:54:53 +0530

    Rectify error in importing Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
+//= require bootstrap-sass/assets/javascripts/bootstrap.js
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
